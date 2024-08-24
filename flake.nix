4dbf035264efe7217dd3e8e5e8ebff9cd9e1c0e9
Matteo Cavestri <matteo.cavestri@protonmail.ch>
2024-08-24 09:04:53 +0200

    added aarch64 support

--- flake.nix
+++ flake.nix
@@ -5,34 +5,36 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
   };
 
-  outputs = { nixpkgs, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-      packages.${system}.cave = pkgs.stdenv.mkDerivation {
-        pname = "cave";
-        version = "0.0.2";
-        src = ./.;
-        buildInputs = [ pkgs.go ];
-        
-        buildPhase = ''
-          export GOCACHE=$(mktemp -d /tmp/go-build-cache-XXXXXX)
-          go build -o cave ./main.go
-        '';
+  outputs = {nixpkgs, ...}: let
+    systems = ["x86_64-linux" "aarch64-linux"];
+  in {
+    packages = nixpkgs.lib.genAttrs systems (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        cave = pkgs.stdenv.mkDerivation {
+          pname = "cave";
+          version = "0.0.3";
+          src = ./.;
+          buildInputs = [pkgs.go];
 
-        installPhase = ''
-          mkdir -p $out/bin
-          cp cave $out/bin/
-        '';
+          buildPhase = ''
+            export GOCACHE=$(mktemp -d /tmp/go-build-cache-XXXXXX)
+            go build -o cave ./main.go
+          '';
 
-        meta = with pkgs.lib; {
-          description = "A CLI tool to manage Nix operations";
-          license = licenses.mit;
-          maintainers = with maintainers; [ Matteo Cavestri ];
+          installPhase = ''
+            mkdir -p $out/bin
+            cp cave $out/bin/
+          '';
+
+          meta = with pkgs.lib; {
+            description = "A CLI tool to manage Nix operations";
+            license = licenses.mit;
+            maintainers = with maintainers; [Matteo Cavestri];
+          };
         };
-      };
-    };
+      }
+    );
+  };
 }
-
